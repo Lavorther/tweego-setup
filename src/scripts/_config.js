16d3ce0adfde52399ffd03fa0fc7a365d64d1b46
Lavorther <lavorther@hotmail.com>
2023-03-26 19:09:58 +0200

    Add default JS configuration script for SugarCube

---
+++ src/scripts/_config.js
@@ -0,0 +1,11 @@
+// Treats all passages as if they have the "nobr" tag
+Config.passages.nobr = true;
+
+// Limit passage history (1 removes history entirely, and also hides history controls)
+// Config.history.maxStates = 1;
+
+// Disable saving
+// Config.saves.slots = 0;
+// Config.saves.isAllowed = function () {
+//     return false;
+// }
